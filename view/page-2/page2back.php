6c533321d9aa9bb7f27bb30bc9111c2a7055c704
Azd Aljayouch <156325800+Dza67@users.noreply.github.com>
2024-03-18 23:49:14 +0100

    maquette v1  backend commande voiture 18/03/2024

---
+++ view/page-2/page2back.php
@@ -0,0 +1,38 @@
+<?php
+
+$servername = "localhost";
+$username = "votre_nom_utilisateur";
+$password = "votre_mot_de_passe";
+$dbname = "nom_de_votre_base_de_donnees";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+}
+
+
+function commanderVoiture($nom, $email, $modele, $couleur) {
+    global $conn;
+    $sql = "INSERT INTO commandes (nom_client, email_client, modele_voiture, couleur_voiture) VALUES ('$nom', '$email', '$modele', '$couleur')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Commande enregistrée avec succès.";
+    } else {
+        echo "Erreur : " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+$nom_client = "Jean Dupont";
+$email_client = "jean.dupont@example.com";
+$modele_voiture = "Voiture modèle X";
+$couleur_voiture = "Bleu";
+
+commanderVoiture($nom_client, $email_client, $modele_voiture, $couleur_voiture);
+
+
+$conn->close();
+?>
